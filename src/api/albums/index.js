e7eb86976e1112900a7f497b2914f7889c0872e0
Sch39 <sukronraezpactoer@gmail.com>
2024-01-06 15:22:05 +0700

    add songs plugin, passed test!

--- src/api/albums/index.js
+++ src/api/albums/index.js
@@ -1,14 +1,14 @@
-const { AlbumPayloadSchema } = require('../../validator/albums/schema');
-const InvariantError = require('../../exceptions/InvariantError');
+// const { AlbumPayloadSchema } = require('../../validator/albums/schema');
+// const InvariantError = require('../../exceptions/InvariantError');
 
-const AlbumsValidator = {
-  validatedAlbumsPayload: (payload) => {
-    const validationResult = AlbumPayloadSchema.validate(payload);
+// const AlbumsValidator = {
+//   validatedAlbumsPayload: (payload) => {
+//     const validationResult = AlbumPayloadSchema.validate(payload);
 
-    if (validationResult.error) {
-      throw new InvariantError(validationResult.error.message);
-    }
-  },
-};
+//     if (validationResult.error) {
+//       throw new InvariantError(validationResult.error.message);
+//     }
+//   },
+// };
 
-module.exports = AlbumsValidator;
+// module.exports = AlbumsValidator;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
+const ClientError = require('./exceptions/ClientError');
 const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
 
 const init = async () => {
+
+  const songService = new SongsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -12,17 +19,41 @@ const init = async () => {
     },
   });
 
-  // await server.register({
-  //   plugin: '',
-  //   options: {
-  //     service: ,
-  //     validator: [],
-  //   },
-  // });
+  await server.register(
+    [
+      {
+        plugin: songs,
+        options: {
+          service: songService,
+          validator: SongsValidator,
+        },
+      },
+    ]
+  );
 
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
 
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    if (response instanceof Error) {
+      const newResponse = h.response({
+        status: 'error',
+        message: response.message,
+      });
+      newResponse.code(response.output.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
